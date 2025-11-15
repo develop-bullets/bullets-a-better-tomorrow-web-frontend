3e22a241234cb8bc36ca1e6ce0d22c90fc08d9a5
phs-playnomm <hspark@playnomm.com>
2025-11-15 14:13:19 +0900

    refactor(performance): [ABT-18] 지영사마 말대로 성능 고려하여 코드 리펙토링

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
+          <span>씨발 지영사마 개새끼 별걸 가지고 트집이네</span>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
